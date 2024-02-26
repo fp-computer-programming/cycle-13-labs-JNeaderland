418fe0d03084b7fb527b2d7afc95e8c554b217c2
JNeaderland <144148081+JNeaderland@users.noreply.github.com>
2024-02-26 11:12:15 -0500

    Add files via upload

---
+++ Lab 13-3 & Lab 13-4.py
@@ -0,0 +1,13 @@
+weekdays = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+weekends = ("Saturday", "Sunday")
+days_of_the_week = (weekdays, weekends)
+print (days_of_the_week)
+
+
+day_date = ("26", "27", "28", "29", "1")
+(Mon, Tue, Wed, Thu, Fri) = day_date
+print (Mon)
+print (Tue)
+print (Wed)
+print (Thu)
+print (Fri)
